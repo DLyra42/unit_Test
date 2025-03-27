f09012f433530c98fb993cdd486b12b0635677b5
Daniel Lyra <dlyradev@gmail.com>
2025-03-27 15:20:29 -0300

    Add unit test ofr calculator functions

---
+++ calc.py
@@ -0,0 +1,17 @@
+# Add function
+def add (x,y):
+    return x + y
+
+# Subtract function
+def subtraction (x,y):
+    return x - y
+
+# Multiply function
+def multiply(x, y):
+    return x*y
+
+# Divide function
+def divide(x,y):
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return x / y 
